594dd731d025f31ea1b62eb4a98598857504048e
brunofaliszewski <bruno0faliszewski@gmail.com>
2020-11-16 19:56:59 +0100

    lekcja4

---
+++ lekcja4/funkcje.py
@@ -0,0 +1,31 @@
+def fmin_val():
+    min_val = int(input('Podaj minimalną wartość losowanej liczby: '))
+
+    return min_val
+
+def fmax_val():
+    max_val = int(input('Podaj maksymalną wartość losowanej liczby: '))
+
+    return max_val
+
+def fnliczb():
+    nliczb = int(input('Podaj liczbe liczb w jednej próbie: '))
+
+    return nliczb
+
+def fnprob():
+    nprob = int(input('Podaj liczbe prób: '))
+
+    return nprob
+
+def wwl(min_val, max_val, nliczb, nprob):
+    from random import randint
+    x = 0
+
+    while x < nprob:
+        y = 0
+        while y < nliczb:
+            print(randint(min_val, max_val), end = ' ')
+            y += 1
+        x += 1
+        print()
---
+++ lekcja4/zad6.py
@@ -0,0 +1,7 @@
+from funkcje import fmin_val, fmax_val, fnliczb, fnprob, wwl
+
+min_val = fmin_val()
+max_val = fmax_val()
+nliczb = fnliczb()
+nprob = fnprob()
+wwl(min_val, max_val, nliczb, nprob)
